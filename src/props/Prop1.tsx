bd1f4bea052de767757bb1a1ea013c9e0b10a87b
MP Polo <mpalexmpalex@gmail.com>
2024-12-09 07:02:22 +0500

    installed typescript

--- src/props/Prop1.tsx
+++ src/props/Prop1.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 function Prop1() {
+  //components stuffs........
+ 
+
   return (
     <p className="text-2 opacity-50 text-secondary">Prop child one component</p>
+    
+    //return stuffs..............
+
+
+
   )
 }
 
